5703a248b7d1541521ac6fd8d1421bae0c76b33e
Indra Kumar Prajapat <indrakumarprajapat@gmail.com>
2025-11-24 16:57:58 +0530

    add

--- pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
-import os from "os";
-
-export default function App({ Component, pageProps }: AppProps) {
-  console.log("Running on server:", os.hostname());
-
-  return <Component {...pageProps} />;
-}
