827bda338825103ade9d143f9001c4df1dc5c8e0
Indra Kumar Prajapat <indrakumarprajapat@gmail.com>
2025-11-28 12:41:19 +0530

    added econfig

---
+++ ecosystem.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  apps: [
+    {
+      name: 'Aavin-SCM-CB',
+      script: 'npm',
+      args: 'start',
+      cwd: '/home/ec2-user/var/www/uat/TestNextProject',
+      instances: 1,
+      autorestart: true,
+      watch: true,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'development',
+                PORT: 3003
+      },
+      env_production: {
+        NODE_ENV: 'production',
+                PORT: 3003
+
+      },
+    },
+  ],
+};
